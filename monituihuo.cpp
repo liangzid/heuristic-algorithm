9032c1582fada9f56928d651070cfe9e9782718d
liangzid <2273067585@qq.com>
2018-11-21 11:31:45 +0800

    ...

--- monituihuo.cpp
+++ monituihuo.cpp
@@ -4,123 +4,53 @@
 #include <vector>
 #include <cmath>
 using namespace std;
-/*============================================================
-Note: This file is the Class of Monituihuo althothgm by Metropolis in 1956.
-You should know: this file is not complete beacuse somewhere You Must have 
-the concrete thing like cost function and so on.
---------------------------------------------------------------
-How to use it? YOU CAN TRY:
 
-//initlize
-Monituihuo alth1(10000,10000,5);
-alth1.vector_best_now=alth1::random_produce_vector();
-bool RUN=1;
-int j=0;
-
-while(RUN)
-{
-	// temprature not change
-	for(int i=0;i<alth1.t_nochange;i++)
-	{
-		vector<int> neww=alth1::produce_new_solution(alth1.vector_now);
-		if(alth1::is_OK(alth1.vec_now,neww,alth1.tem))
-			alth1.vec_now=neww;
-
-		// exit if to many time no change;
-		;
-
-	}
-
-	//temprature decrease
-	alth1.tem_decrease();
-	j++;
-	if (j>alth1.num_of_diedai||alth1.tem<1)
-	{
-		RUN=0;
-		alth1.final_vector=alth1.vector_now;
-		alth1.final_value=alth1::cost_function(alth1.final_vector);
-	}
-
-}
-*/
-
-
-class Monituihuo()
-{
-public:
-	vector<int> vector_now;
-	vector<int> final_vector;
-	double value_now;
-	double final_value;
-	int num_of_diedai;
-	string tem_method="NOTSET";
-	const double init_temprature;
-	double tem;
-	int t_nochange;
-
-
-	Monituihuo(double init_tem,int num_of_diedaii,int t);
-
-
-	vector<int> random_produce_vector();
-	vector<int> produce_new_solution(std::vector<int> now, /*other parameter*/ );
-	double cost_function(vector<int> vec);
-	bool is_OK(vector<int> vec_now,vector<int> vec_new, double tem);
-	vector<int> getbestvec();
-	double getbestvalue();
-	double gettem();
-	int getdiedaitime();
-
-	double tem_decrease();
-
-private:
-	
-	
-};
 
 Monituihuo::Monituihuo(double init_tem,int num_of_diedaii,int t)
 {
 	init_temprature=init_tem;
 	num_of_diedai=num_of_diedaii;
 	t_nochange=t;
+	atio=0.95;
 }
-vector<int> random_produce_vector()
+Monituihuo::Monituihuo(double init_tem,int num_of_diedaii,int t,double atio)
+{
+	init_temprature=init_tem;
+	num_of_diedai=num_of_diedaii;
+	t_nochange=t;
+	this->atio=atio;
+}
+void Monituihuo::random_produce_vector()
 {
 	vector<int> result;
-	int x;
-	for(int i=0;i<num_to_init;i++)
-	{
-		x=rand_num();
-		result.push_back(x)
-	}
-
-	return ;
+	//this where needs some process which depends on concrete problem, so i donnot write here.
+	this->vector_now=result;
 }
 
-vector<int> produce_new_solution(vector<int> now)
+void Monituihuo::produce_new_solution()
 {
 	vector<int> neww;
-
-	return neww;
-
+	//here also need some process;
+	this->vector_new=neww;
 }
 
-double cost_function(vector<int> vec)
+double Monituihuo::cost_function(vector<int> vec)
 {
+	//here also need some process;
 	return 0;
 }
 
-bool is_OK(vector<int> now,vector<int> neww, double tem)
+bool Monituihuo::is_OK()
 {
 	double zelta;
-	zelta=cost_function(neww)-cost_function(now);
+	zelta=this->cost_function(this->vector_new)-cost_function(this->vector_now);
 	double P;
 	if (zelta<0)
 		return 1;
 	else 
 	{
 		P=exp(-zelta/tem);
-		if(P>((rand()%1001)/10000.0))
+		if(P>((rand()%1001)/10000.0)) //generate rand number in [0,1].
 			return 1;
 
 	}
@@ -128,20 +58,29 @@ bool is_OK(vector<int> now,vector<int> neww, double tem)
 
 }
 
-vector<int> getbestvec()
+vector<int> Monituihuo::getbestvec()
 {
 	return vector_best_now;
 }
-double getbestvalue()
+double Monituihuo::getbestvalue()
 {
 	return value_best_now;
 }
-double gettem()
+double Monituihuo::gettem()
 {
 	return tem;
 }
-int getdiedaitime()
+int Monituihuo::getdiedaitime()
 {
 	return num_of_diedai;
 }
 
+void Monituihuo::tem_decrease()
+{
+	//you change here for more thing you want. for example:
+	//this.tem -=this.atio;
+	//also you can change the value of atio in Monituihuo::Monituihuo();or you can write it in the construct function para.
+
+	this->tem *= this->atio;
+
+}
--- zhinengyouhua.h
+++ zhinengyouhua.h
@@ -2,3 +2,88 @@
 #include<vector>
 #include<string>
 #include<cmath>
+using namespace std;
+
+/*============================================================
+Note: This file is the Class of Monituihuo althothgm by Metropolis in 1956.
+You should know: this file is not complete beacuse somewhere You Must have 
+the concrete thing like cost function and so on.
+--------------------------------------------------------------
+How to use it? YOU CAN TRY:
+
+//initlize
+Monituihuo alth1(10000,10000,5);
+alth1.vector_now=Monituihuo.random_produce_vector();
+bool RUN=1;
+int j=0;
+
+while(RUN)
+{
+	// temprature not change
+	for(int i=0;i<alth1.t_nochange;i++)
+	{
+		Monituihuo.produce_new_solution();
+		if(Monituihuo.is_OK(alth1.vec_now,neww,alth1.tem))
+			alth1.vector_now=alth1.vector_new;
+
+		// exit if to many time no change;
+		;
+
+	}
+
+	//temprature decrease
+	alth1.tem_decrease();
+	j++;
+	if (j>alth1.num_of_diedai||alth1.tem<1)
+	{
+		RUN=0;
+		alth1.final_vector=alth1.vector_now;
+		alth1.final_value=alth1::cost_function(alth1.final_vector);
+	}
+
+}
+*/
+
+#ifndef MONITUIHUO_H
+#define MONITUIHUO_H
+//declare a class to this algorithm.
+class Monituihuo()
+{
+public:
+	vector<int> vector_now;
+	vector<int> final_vector;
+	vector<int> vector_new;
+	double value_now;
+	double final_value;
+	
+	int num_of_diedai;
+	string tem_method="NOTSET";
+	double init_temprature;
+	
+	double tem;
+	double atio;
+	int t_nochange;
+
+    //construt function
+	Monituihuo(double init_tem,int num_of_diedaii,int t);
+    Monituihuo(double init_tem,int num_of_diedaii,int t,double atio);
+	void random_produce_vector();
+	void produce_new_solution();
+	
+	double cost_function(vector<int> vec);
+	
+	bool is_OK();
+
+	vector<int> getbestvec();
+	double getbestvalue();
+	double gettem();
+	int getdiedaitime();
+
+	void tem_decrease();
+
+private:
+	
+	
+};
+
+#endif
